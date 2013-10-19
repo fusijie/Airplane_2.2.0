24ab3112bf9b97d18590ca334f3cd426af12a09b
Jacky <fusijie@vip.qq.com>
2013-10-19 12:01:44 +0800

    Add Copyright

--- Classes/ControlLayer.h
+++ Classes/ControlLayer.h
@@ -1,3 +1,16 @@
+/*******************************************************************************
+	Author						: JackyFu
+	Email                   	: fusijie@vip.qq.com
+	QQ  						: 754505629
+	Blog						: http://blog.csdn.net/jackystudio
+	All Rights Reserved
+	
+	This piece of code does not have any registered copyright and is free to be 
+	used as necessary. The user is free to modify as per the requirements. As a
+	fellow developer, all that I expect and request for is to be given the 
+	credit for intially developing this reusable code by not removing my name as 
+	the author.
+*******************************************************************************/
 #pragma once
 
 #include "cocos2d.h"
